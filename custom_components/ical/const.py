e5e3fb7c563c4e5ec7188dfaa073961aec9ae146
Frazou1 <89617565+Frazou1@users.noreply.github.com>
2025-02-05 09:10:27 -0500

    Update const.py

--- custom_components/ical/const.py
+++ custom_components/ical/const.py
@@ -5,6 +5,7 @@ DOMAIN = "ical"
 
 CONF_MAX_EVENTS = "max_events"
 CONF_DAYS = "days"
+CONF_FILTER_KEYWORD = "filter_keyword"  # Nouvelle constante pour le filtre sur le sommaire
 
 ICON = "mdi:calendar"
 DEFAULT_NAME = "iCal Sensor filter custom"
