d25e5f417a9918a59ffc5bff48355d9c282ee1f2
Frazou1 <89617565+Frazou1@users.noreply.github.com>
2025-02-11 15:53:52 -0500

    Update __init__.py

--- custom_components/ical_custom/__init__.py
+++ custom_components/ical_custom/__init__.py
@@ -10,6 +10,19 @@ from dateutil.tz import gettz, tzutc
 import icalendar
 import voluptuous as vol
 
+_LOGGER = logging.getLogger(__name__)
+
+# Patch pour compatibilité avec la nouvelle version d’icalendar
+if not hasattr(icalendar.timezone, 'equivalent_timezone_ids_result'):
+    def equivalent_timezone_ids_result(tzid):
+        """
+        Fonction de remplacement pour equivalent_timezone_ids_result.
+        Retourne simplement le tzid dans une liste.
+        """
+        return [tzid]
+    icalendar.timezone.equivalent_timezone_ids_result = equivalent_timezone_ids_result
+    _LOGGER.debug("Patched icalendar.timezone.equivalent_timezone_ids_result")
+
 from homeassistant.components.calendar import CalendarEvent
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import CONF_NAME, CONF_URL, CONF_VERIFY_SSL
@@ -17,17 +30,10 @@ from homeassistant.core import HomeAssistant
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 from homeassistant.util import Throttle, dt as dt_util
 
-from .const import CONF_DAYS, CONF_MAX_EVENTS, DOMAIN
-
-_LOGGER = logging.getLogger(__name__)
-
-
 CONFIG_SCHEMA = vol.Schema({DOMAIN: vol.Schema({})}, extra=vol.ALLOW_EXTRA)
 
-# TODO List the platforms that you want to support.
-# For your initial PR, limit it to 1 platform.
+# Liste des plateformes supportées (pour cet exemple, uniquement sensor)
 PLATFORMS = ["sensor"]
-# PLATFORMS = ["sensor"]
 
 MIN_TIME_BETWEEN_UPDATES = timedelta(seconds=120)
 
@@ -43,8 +49,6 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     _LOGGER.debug(
         "Running init async_setup_entry for calendar %s", config.get(CONF_NAME)
     )
-    # TODO Store an API object for your platforms to access
-    # hass.data[DOMAIN][entry.entry_id] = MyApi(...)
     if DOMAIN not in hass.data:
         hass.data[DOMAIN] = {}
     hass.data[DOMAIN][config.get(CONF_NAME)] = ICalEvents(hass=hass, config=config)
@@ -104,7 +108,6 @@ class ICalEvents:
 
                 if event["start"] < end_date and event["end"] > start_date:
                     _LOGGER.debug("... and it has")
-                    # strongly type class fix
                     events.append(
                         CalendarEvent(
                             event["start"],
@@ -114,7 +117,6 @@ class ICalEvents:
                             event["location"],
                         )
                     )
-                    # events.append(event)
         return events
 
     @Throttle(MIN_TIME_BETWEEN_UPDATES)
@@ -127,14 +129,13 @@ class ICalEvents:
                 text = f.read()
         else:
             if parts.scheme == "webcal":
-                # There is a potential issue here if the real URL is http, not https
+                # Remplacer webcal par https
                 self.url = parts.geturl().replace("webcal", "https", 1)
             session = async_get_clientsession(self.hass, verify_ssl=self.verify_ssl)
             async with session.get(self.url) as response:
                 text = await response.text()
         if text is not None:
-            # Some calendars are for some reason filled with NULL-bytes.
-            # They break the parsing, so we get rid of them
+            # Certains calendriers contiennent des NULL-bytes qu'il faut retirer
             event_list = icalendar.Calendar.from_ical(text.replace("\x00", ""))
             start_of_events = dt_util.start_of_local_day()
             end_of_events = dt_util.start_of_local_day() + timedelta(days=self.days)
@@ -148,7 +149,7 @@ class ICalEvents:
             for event in self.calendar:
                 if event["end"] > dt_util.now() and not found_next_event:
                     _LOGGER.debug(
-                        "Event %s it the first event with end in the future: %s",
+                        "Event %s is the first event with end in the future: %s",
                         event["summary"],
                         event["end"],
                     )
@@ -157,33 +158,22 @@ class ICalEvents:
 
     async def _ical_parser(self, calendar, from_date, to_date):
         """Return a sorted list of events from a icalendar object."""
-
         events = []
 
         for event in calendar.walk("VEVENT"):
-            # RRULEs turns out to be harder than initially thought.
-            # This is mainly due to pythons handling of TZ-naive and TZ-aware timestamps, and the inconsistensies
-            # in the way RRULEs are implemented in the icalendar library.
             if "RRULE" in event:
-                # _LOGGER.debug("RRULE in event: %s", str(event["SUMMARY"]))
                 rrule = event["RRULE"]
-                # Since we dont get both the start and the end in a single object, we need to generate two lists,
-                # One of all the DTSTARTs and another list of all the DTENDs
                 start_rules = rruleset()
                 end_rules = rruleset()
 
                 if "UNTIL" in rrule:
                     try:
-                        # Just ignore events that ended a long time ago
                         if rrule["UNTIL"][0] < from_date - timedelta(days=30):
-                            # _LOGGER.debug("Old event 1 %s - ended %s", event["SUMMARY"], str(rrule["UNTIL"][0]))
                             continue
                     except Exception:
                         pass
 
                     _LOGGER.debug("UNTIL in rrule: %s", str(rrule["UNTIL"]))
-                    # Ensure that UNTIL is tz-aware and in UTC
-                    # (Not all icalendar implements this correctly)
                     until = await self._ical_date_fixer(rrule["UNTIL"], "UTC")
                     rrule["UNTIL"] = [until]
                 else:
@@ -197,7 +187,6 @@ class ICalEvents:
                 if "DTEND" not in event:
                     _LOGGER.debug("Event found without end datetime")
                     if self.all_day:
-                        # if it's an all day event with no endtime listed, we'll assume it ends at 23:59:59
                         _LOGGER.debug(
                             f"Event {event['SUMMARY']} is flagged as all day, with a start time of {dtstart}."
                         )
@@ -214,14 +203,11 @@ class ICalEvents:
                         event["DTEND"].dt, dt_util.DEFAULT_TIME_ZONE
                     )
 
-                # So hopefully we now have a proper dtstart we can use to create the start-times according to the rrule
-                # _LOGGER.debug("RRulestr %s", rrule.to_ical().decode("utf-8"))
                 try:
                     start_rules.rrule(
                         rrulestr(rrule.to_ical().decode("utf-8"), dtstart=dtstart)
                     )
                 except Exception as e:
-                    # If this fails, move on to the next event
                     _LOGGER.error(
                         "Exception %s in start_rules.rrule: %s - Start: %s - RRule: %s",
                         str(e),
@@ -230,15 +216,12 @@ class ICalEvents:
                         str(event["RRULE"]),
                     )
                     continue
-                # _LOGGER.debug("Start rules %s", str(list(start_rules)))
 
-                # ... And the same for end_rules
                 try:
                     end_rules.rrule(
                         rrulestr(rrule.to_ical().decode("utf-8"), dtstart=dtend)
                     )
                 except Exception as e:
-                    # If this fails, just use the start-rules
                     _LOGGER.error(
                         "Exception %s in end_rules.rrule: %s - End: %s - RRule: %s",
                         str(e),
@@ -248,9 +231,6 @@ class ICalEvents:
                     )
                     end_rules = start_rules
 
-                # EXDATEs are hard to parse.  They might be a list, or just a single object.
-                # They might contain TZ-data, they might not...
-                # We just do our best, and will catch the exception when it fails and move on the the next event.
                 try:
                     if "EXDATE" in event:
                         if isinstance(event["EXDATE"], list):
@@ -273,8 +253,6 @@ class ICalEvents:
                     )
                     continue
 
-                # Lets get all RRULE-generated events which will start 7 days before today and end before to_date
-                # to ensure we are catching (most) recurring events that might already have started.
                 try:
                     starts = start_rules.between(
                         after=(from_date - timedelta(days=7)), before=to_date
@@ -293,59 +271,42 @@ class ICalEvents:
                     )
                     continue
 
-                # _LOGGER.debug("Starts: %s", str(starts))
-                # We might get RRULEs that does not fall within the limits above, lets just skip them
                 if len(starts) < 1:
                     _LOGGER.debug("Event does not happen within our limits")
                     continue
 
-                # It has to be a better way to do this...But at least it seems to work for now.
                 ends.reverse()
                 for start in starts:
-                    # Sometimes we dont get the same number of starts and ends...
                     if len(ends) == 0:
                         continue
                     end = ends.pop()
                     event_dict = self._ical_event_dict(start, end, from_date, event)
-
                     if event_dict:
                         events.append(event_dict)
 
                 _LOGGER.debug("Done parsing RRULE")
 
             else:
-                # Let's use the same magic as for rrules to get this (as) right (as possible)
                 try:
-                    # Just ignore events that ended a long time ago
-                    if "DTEND" in event and event[
-                        "DTEND"
-                    ].dt.date() < from_date.date() - timedelta(days=30):
-                        # _LOGGER.debug("Old event 1 %s - ended %s", event["SUMMARY"], str(event["DTEND"].dt))
+                    if "DTEND" in event and event["DTEND"].dt.date() < from_date.date() - timedelta(days=30):
                         continue
                 except Exception:
-                    # _LOGGER.debug("1: %s", str(e))
                     pass
                 try:
-                    if "DTEND" in event and event[
-                        "DTEND"
-                    ].dt < from_date.date() - timedelta(days=30):
-                        # _LOGGER.debug("Old event 2 %s - ended %s", event["SUMMARY"], str(event["DTEND"].dt))
+                    if "DTEND" in event and event["DTEND"].dt < from_date.date() - timedelta(days=30):
                         continue
                 except Exception:
-                    # _LOGGER.debug("2: %s", str(e))
                     pass
 
                 _LOGGER.debug("DTSTART in event: {}".format(event["DTSTART"].dt))
                 dtstart = await self._ical_date_fixer(
                     event["DTSTART"].dt, dt_util.DEFAULT_TIME_ZONE
                 )
-
                 start = dtstart
 
                 if "DTEND" not in event:
                     _LOGGER.debug("Event found without end datetime")
                     if self.all_day:
-                        # if it's an all day event with no endtime listed, we'll assume it ends at 23:59:59
                         _LOGGER.debug(
                             f"Event {event['SUMMARY']} is flagged as all day, with a start time of {start}."
                         )
@@ -371,12 +332,9 @@ class ICalEvents:
 
     def _ical_event_dict(self, start, end, from_date, event):
         """Ensure that events are within the start and end."""
-
-        # Skip this event if it's in the past
         if end.date() < from_date.date():
             _LOGGER.debug("This event has already ended")
             return None
-        # Ignore events that ended this midnight.
         if (
             end.date() == from_date.date()
             and end.hour == 0
@@ -409,11 +367,9 @@ class ICalEvents:
 
         _LOGGER.debug("Fixing date: %s in TZ %s", str(indate), str(timezone))
 
-        # Indate can be a single entry or a list with one item...
         if isinstance(indate, list):
             indate = indate[0]
 
-        # Indate can be a date without time...
         if not isinstance(indate, datetime):
             try:
                 self.all_day = True
@@ -432,19 +388,10 @@ class ICalEvents:
 
     def _date_replace(self, indate: datetime, timezone):
         """Replace tzinfo in a datetime object."""
-
-        # Indate can be TZ naive
         if indate.tzinfo is None or indate.tzinfo.utcoffset(indate) is None:
-            # _LOGGER.debug("TZ-Naive indate: %s Adding TZ %s", str(indate), str(gettz(str(timezone))))
-            # tz = pytz.timezone(str(timezone))
-            # indate = tz.localize(indate)
             return indate.replace(tzinfo=gettz(str(timezone)))
-        # Rules dont play well with pytz
-        # _LOGGER.debug("Tzinfo 1: %s", str(indate.tzinfo))
         if not str(indate.tzinfo).startswith("tzfile"):
-            # _LOGGER.debug("Pytz indate: %s. replacing with tz %s", str(indate), str(gettz(str(indate.tzinfo))))
             return indate.replace(tzinfo=gettz(str(indate.tzinfo)))
         if str(indate.tzinfo).endswith("/UTC"):
             return indate.replace(tzinfo=tzutc)
-        # _LOGGER.debug("Tzinfo 2: %s", str(indate.tzinfo))
         return None
